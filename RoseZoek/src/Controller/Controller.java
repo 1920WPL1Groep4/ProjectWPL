ce01021035f6b4d766d4d262dedf73699770aa65
Femke Vermander <femke.vermander@student.vives.be>
2020-03-04 15:06:13 +0100

    GuiVersie0.3

--- RoseZoek/src/Controller/Controller.java
+++ RoseZoek/src/Controller/Controller.java
@@ -8,12 +8,12 @@ import javafx.scene.input.MouseEvent;
 import java.sql.*;
 
 public class Controller {
-    public Label lblPlantResultaat; //Label naam waar het resultaat
+    public Label lblPlantResultaat; //Label waar het resultaat van de gevonden plant in komt.
 
-    public Spinner sType;       //Spinner naam voor het type van de plant.
-    public Spinner sFamilie;    //Spinner naam voor de familie van de plant.
-    public Spinner sGeslacht;   //Spinner naam voor het geslacht van de plant.
-    public Spinner sSoort;      //Spinner naam voor de soort van de plant.
+    public Spinner sType;       //Spinner voor het type van de plant.
+    public Spinner sFamilie;    //Spinner voor de familie van de plant.
+    public Spinner sGeslacht;   //Spinner voor het geslacht van de plant.
+    public Spinner sSoort;      //Spinner voor de soort van de plant.
 
     public TextArea txtUitkomst;    //Dit is de naam van de textarea waar de uitkomst van de zoek opdracht getoont wordt.
 
@@ -22,6 +22,27 @@ public class Controller {
     public Button btnVoorwaards;    //Button naam voor de button Voorwaards.
 
     public TitledPane tlpExtraEigenschappen;    //TitlePane voor de extra eigenschappen waar opgezocht kan worden.
+
+    public Slider sldrBladgrootte;
+
+    public CheckBox cbBloeiJan; //Checkbox voor de bloeihoogte in de maand januari.
+    public CheckBox cbBloeiFeb; //Checkbox voor de bloeihoogte in de maand februari.
+    public CheckBox cbBloeiMaa; //Checkbox voor de bloeihoogte in de maand maart.
+    public CheckBox cbBloeiApr; //Checkbox voor de bloeihoogte in de maand april.
+    public CheckBox cbBloeiMei; //Checkbox voor de bloeihoogte in de maand mei.
+    public CheckBox cbBloeiJun; //Checkbox voor de bloeihoogte in de maand juni.
+    public CheckBox cbBloeiJul; //Checkbox voor de bloeihoogte in de maand juli.
+    public CheckBox cbBloeiAug; //Checkbox voor de bloeihoogte in de maand augustus.
+    public CheckBox cbBloeiSep; //Checkbox voor de bloeihoogte in de maand september.
+    public CheckBox cbBloeiOkt; //Checkbox voor de bloeihoogte in de maand oktober.
+    public CheckBox cbBloeiNov; //Checkbox voor de bloeihoogte in de maand november.
+    public CheckBox cbBloeiDec; //Checkbox voor de bloeihoogt in de maand december.
+
+    public TextField txtBladhJan;   //Textfield voor de bladhoogte in de maand januari.
+    public TextField txtBladhFeb;   //Textfield voor de bladhoogte in de maand februari.
+    public TextField txtBladhMaa;   //Textfield voor de bladhoogte in de maand maart.
+    public TextField txtBladhApr;   //Textfield voor de bladhoogte in de maand april.
+    public TextField txtBladhMei;   //Textfield voor de bladhoogte in de maand mei.
 
     public ComboBox cmbbBezonning;          //Combox naam voor bezonning.
     public ComboBox cmbbOntwikkelingsS;     //Combox naam voor ontwikkelingssnelheid.
@@ -33,6 +54,12 @@ public class Controller {
     public ComboBox cmbbGrondstof;          //Combox naam voor grondstof.
     public ComboBox cmbbVochtbehoefte;      //Combox naam voor vochtbehoefte.
     public ComboBox cmbbVoedingsbehoefte;   //Combox naam voor voedingsbehoefte.
+
+
+
+
+
+
 
 
     private Connection dbconnection;
