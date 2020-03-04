3af8486ecb67041a391d954e14a5c1c647a19816
Femke Vermander <femke.vermander@student.vives.be>
2020-03-04 13:36:37 +0100

    GuiVersie0.2

--- RoseZoek/src/Controller/Controller.java
+++ RoseZoek/src/Controller/Controller.java
@@ -2,36 +2,37 @@ package Controller;
 
 import dbo.Database;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.sql.*;
 
 public class Controller {
+    public Label lblPlantResultaat; //Label naam waar het resultaat
 
-    //Dit zijn de namen van de labels van plant, type, familie, geslacht en soort.
-    public Label lblPlant;
-    public Label lblType;
-    public Label lblFamilie;
-    public Label lblGelsacht;
-    public Label lblSoort;
+    public Spinner sType;       //Spinner naam voor het type van de plant.
+    public Spinner sFamilie;    //Spinner naam voor de familie van de plant.
+    public Spinner sGeslacht;   //Spinner naam voor het geslacht van de plant.
+    public Spinner sSoort;      //Spinner naam voor de soort van de plant.
 
-    //Dit zijn de namen van de spinners van soort, geslacht, familie en type.
-    public Spinner sSoort;
-    public Spinner sGeslacht;
-    public Spinner sFamilie;
-    public Spinner sType;
+    public TextArea txtUitkomst;    //Dit is de naam van de textarea waar de uitkomst van de zoek opdracht getoont wordt.
 
-    //Dit is de naam van de textarea waar de uitkomst van de zoek opdracht getoont wordt.
-    public TextArea txtUitkomst;
+    public Button btnZoeken;        //Button naam voor de button zoeken.
+    public Button btnAchterwaards;  //Button naam voor de button achterwaards.
+    public Button btnVoorwaards;    //Button naam voor de button Voorwaards.
 
-    //Dit zijn de namen van de button.
-    public Button btnZoeken;
-    public Button btnAchterwaards;
-    public Button btnVoorwaards;
+    public TitledPane tlpExtraEigenschappen;    //TitlePane voor de extra eigenschappen waar opgezocht kan worden.
+
+    public ComboBox cmbbBezonning;          //Combox naam voor bezonning.
+    public ComboBox cmbbOntwikkelingsS;     //Combox naam voor ontwikkelingssnelheid.
+    public ComboBox cmbbSociabiliteit;      //Combox naam voor sociabiliteit.
+    public ComboBox cmbbLevensduur;         //Combox naam voor levensduus.
+    public ComboBox cmbbBladvorm;           //Combox naam voor bladvorm.
+    public ComboBox cmbbRatio;              //Combox naam voor ratio bloei en blad.
+    public ComboBox cmbbSpruitfenologie;    //Combox naam voor spruitfenologie.
+    public ComboBox cmbbGrondstof;          //Combox naam voor grondstof.
+    public ComboBox cmbbVochtbehoefte;      //Combox naam voor vochtbehoefte.
+    public ComboBox cmbbVoedingsbehoefte;   //Combox naam voor voedingsbehoefte.
 
 
     private Connection dbconnection;
@@ -49,14 +50,15 @@ public class Controller {
         System.out.println("just for testing purposes");
     }
 
-    //Button die ervoorzorgt dat er een zoek resultaat wordt weergegeven in de textarea
+    //Button die ervoor zorgt dat er een zoek resultaat wordt weergegeven in de textarea
     public void clicked_Zoeken(MouseEvent mouseEvent) {
     }
 
-
+    //Gaat een plant terug wanneer er op de button btnAchterwaards gedrukt wordt.
     public void clicked_Achterwaards(MouseEvent mouseEvent) {
     }
 
+    //Gaat een plant naar voor wanneer er op de button btnVoorwaards gedrukt wordt.
     public void clicked_Voorwaards(MouseEvent mouseEvent) {
     }
 }
