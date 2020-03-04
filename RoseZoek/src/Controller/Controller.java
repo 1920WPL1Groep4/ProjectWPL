744a25d535a765c909bca85352fc1fa907906071
Femke Vermander <femke.vermander@student.vives.be>
2020-03-04 10:23:02 +0100

    GuiVersie0.1

--- RoseZoek/src/Controller/Controller.java
+++ RoseZoek/src/Controller/Controller.java
@@ -2,10 +2,36 @@ package Controller;
 
 import dbo.Database;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 
 import java.sql.*;
 
 public class Controller {
+
+    //Dit zijn de namen van de labels van plant, type, familie, geslacht en soort.
+    public Label lblPlant;
+    public Label lblType;
+    public Label lblFamilie;
+    public Label lblGelsacht;
+    public Label lblSoort;
+
+    //Dit zijn de namen van de spinners van soort, geslacht, familie en type.
+    public Spinner sSoort;
+    public Spinner sGeslacht;
+    public Spinner sFamilie;
+    public Spinner sType;
+
+    //Dit is de naam van de textarea waar de uitkomst van de zoek opdracht getoont wordt.
+    public TextArea txtUitkomst;
+
+    //Dit zijn de namen van de button.
+    public Button btnZoeken;
+    public Button btnAchterwaards;
+    public Button btnVoorwaards;
 
 
     private Connection dbconnection;
@@ -21,6 +47,17 @@ public class Controller {
 
     public void btn(ActionEvent actionEvent) {
         System.out.println("just for testing purposes");
+    }
+
+    //Button die ervoorzorgt dat er een zoek resultaat wordt weergegeven in de textarea
+    public void clicked_Zoeken(MouseEvent mouseEvent) {
+    }
+
+
+    public void clicked_Achterwaards(MouseEvent mouseEvent) {
+    }
+
+    public void clicked_Voorwaards(MouseEvent mouseEvent) {
     }
 }
 
