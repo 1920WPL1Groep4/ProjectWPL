804b74e00cecedac489e514bcc7267f896714e6e
Reynaert Geoffrey <Geoffrey.reynaert@student.vives.be>
2020-03-07 17:32:47 +0100

    Plant classe bijgewertk

--- RoseZoek/src/Model/Plant.java
+++ RoseZoek/src/Model/Plant.java
@@ -2,24 +2,33 @@ package Model;
 
 public class Plant {
     String sNaam;
-    String sFamilie;
     String sType;
+    String sFamilie;
+    String sGeslacht;
+    String sSoort;
+    String sVariant;
 
-    public Plant(String sNaam, String sFamilie,String sType) {
+    public Plant(String sNaam, String sType, String sFamilie) {
         this.sNaam = sNaam;
-        this.sFamilie = sFamilie;
         this.sType = sType;
-
+        this.sFamilie = sFamilie;
+        this.sGeslacht = sGeslacht;
+        this.sSoort = sSoort;
+        this.sVariant = sVariant;
     }
-//functie geoffrey
-    public String myToString(){
-        String sAnswer = "plantnaam = "+sNaam+ "\r\n"+ "Familie = "+sFamilie+"\r\n"+ "Type = "+sType;
+
+    //functie geoffrey
+    public String myToString() {
+        String sAnswer = "plantnaam = " + sNaam + "\r\n" + "Familie = " + sFamilie + "\r\n" + "Type = " + sType;
         return sAnswer;
     }
-//functie bram
-    public String MyPlantNameToString(){
-        String sAnswer = "plantnaam: " +sNaam;
-       return sAnswer;
-    };
+
+    //functie bram
+    public String MyPlantNameToString() {
+        String sAnswer = "plantnaam: " + sNaam;
+        return sAnswer;
+    }
+
+    ;
 
 }
